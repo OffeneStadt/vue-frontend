c588074baf882ec5cf41b7115c9bc86596d72771
Tom Friedel <friedel@we-build.city>
2019-10-02 15:04:45 +0200

    - added partner

---
+++ src/components/Partner.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="screenshot-area section-padding pt-100 fix" id="partner">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title text-center">
+                        <div class="title-icon"></div>
+                        <h2 class="title">Partner</h2>
+                        <p class="paragraph">Unsere unterstützer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row mtn-30">
+                <div v-for="(parter, logo) in parter" :key="logo" class="col-lg-4 col-md-4">
+                    <div class="screenshot-item">
+                        <a href="#"><img :src="parter.logoImg" alt="screenshot thumb"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Partner',
+        data (){
+            return {
+                parter: [
+                    {logoImg: require("../assets/img/partner/odaki.png")},
+                ]
+            }
+        }
+    }
+
+</script>
