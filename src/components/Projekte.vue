8352166db8e9bb752d683de8ec20b92be55d620b
Tom Friedel <friedel@we-build.city>
2019-10-07 16:27:34 +0200

    - added projects

--- src/components/Projekte.vue
+++ src/components/Projekte.vue
@@ -14,14 +14,16 @@
                 <!-- feature single item start -->
                 <div v-for="(feature, features) in features" :key="features" class="col-lg-3 col-md-6">
                     <div class="feature-item">
-                        <div>
-                        <!--<div class="feature-icon">-->
-                            <img :src="feature.thumb" alt="team thumb">
-                        </div>
-                        <div class="feature-content">
-                            <h6>{{ feature.title }}</h6>
-                            <p>{{ feature.desc }}</p>
-                        </div>
+                        <a :href="feature.link" target="_blank">
+                            <div>
+                            <!--<div class="feature-icon">-->
+                                <img :src="feature.thumb" alt="team thumb">
+                            </div>
+                            <div class="feature-content">
+                                <h6>{{ feature.title }}</h6>
+                                <p>{{ feature.desc }}</p>
+                            </div>
+                        </a>
                     </div>
                 </div>
                 <!-- feature single item start -->
@@ -38,10 +40,22 @@ export default {
     data () {
         return {
             features: [
-                {title:"OFFENER HAUSHALT", desc:"Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet.", thumb: require("../assets/img/projekte/offenerhaushalt.png")},
-                {title:"URBAN PLATFORM", desc:"Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet.", thumb: require("../assets/img/projekte/urbanplatform.png")},
-                {title:"LUFTDATEN.INFO", desc:"Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet.", thumb: require("../assets/img/projekte/luftdaten.png")},
-                {title:"ODAKI", desc:"Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet.", thumb: require("../assets/img/projekte/odaki.png")}
+                {title:"OFFENER HAUSHALT", link:"https://offenerhaushalt.de/", desc:"Offene Verwaltung", thumb: require("../assets/img/projekte/offenerhaushalt.png")},
+                {title:"URBAN PLATFORM", link:"", desc:"Offene Verwaltung", thumb: require("../assets/img/projekte/urbanplatform.png")},
+                {title:"LUFTDATEN.INFO", link:"https://luftdaten.info", desc:"Offene Gesellschaft", thumb: require("../assets/img/projekte/luftdaten.png")},
+                {title:"ODAKI", link:"", desc:"Offene Verwaltung", thumb: require("../assets/img/projekte/odaki.png")},
+                {title:"OFFENE KOMMUNE.NRW", link:"", desc:"Offene Verwaltung", thumb: require("../assets/img/projekte/OKNRW.png")},
+                {title:"CODING DA VINCI", link:"", desc:"Offene Kultur", thumb: require("../assets/img/projekte/codingdavinci.png")},
+                {title:"KARTEN AUS FREIEN DATEN", link:"", desc:"Offene Gesellschaft", thumb: require("../assets/img/projekte/kartenworkshop.png")},
+                {title:"JUGEND HACKT", link:"", desc:"Offene Gesellschaft", thumb: require("../assets/img/projekte/jugendhackt.png")},
+                {title:"GOVDATA", link:"", desc:"Offene Verwaltung", thumb: require("../assets/img/projekte/govdata.png")},
+                {title:"FRAGDENSTAAT", link:"", desc:"Offene Verwaltung", thumb: require("../assets/img/projekte/fragdenstaat.png")},
+                {title:"EINFACH KAUFEN", link:"", desc:"Offene Gesellschaft", thumb: require("../assets/img/projekte/einfachkaufen.png")},
+                {title:"TRINKWASSER APP", link:"http://codeforhamburg.org/trinkwasser/#12/53.5411/9.9780", desc:"Offene Verwaltung", thumb: require("../assets/img/projekte/trinkwasserapp.jpg")},
+                {title:"WIR VERSILBERN DAS NETZ", link:"", desc:"Offene Gesellschaft", thumb: require("../assets/img/projekte/Einsamkeit.png")},
+                {title:"LANDKREIS MARBURG-BIEDENKOPF", link:"", desc:"Offene Verwaltung", thumb: require("../assets/img/projekte/offenestadt.png")},
+                {title:"OPEN KNOWLEDGE FOUNDATION", link:"https://okfn.de/", desc:"Offene Gesellschaft", thumb: require("../assets/img/projekte/offenestadt.png")},
+                {title:"ATOMMÜLLATLAS", link:"https://atommuell-atlas.de/", desc:"Offene Gesellschaft", thumb: require("../assets/img/projekte/atommuell_logo.png")},
             ]
         }
     }
