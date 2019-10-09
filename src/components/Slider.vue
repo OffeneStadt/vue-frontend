ecfdfbc941cb3372ba8e9ad3a109f831f3334cfe
Tom Friedel <friedel@we-build.city>
2019-10-09 12:07:33 +0200

    - fixed registration link

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -20,8 +20,7 @@
                                 <h2 class="slider-title">{{ slider.sliderTitle }}</h2>
                                 <p class="slider-desc" style="background-color: rgb(31, 68, 108,0.8); padding: 10px">{{ slider.sliderDesc }}</p>
                                 <div class="slider-btn">
-                                    <a href="#" class="read-more active"><i class="fa fa-meetup"></i> Anmeldung</a>
-                                    <a href="#" class="read-more"><i class="fa fa-heart"></i> Open Apps</a>
+                                    <a href="https://www.koerber-stiftung.de/?id=2393" class="read-more active" target="_blank"><i class="fa fa-meetup"></i> Anmeldung</a>
                                 </div>
                             </div>
                         </div>
