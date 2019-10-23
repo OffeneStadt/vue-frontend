12328076bc7185b7ab1a9a90268ee842d97e385c
Tom Friedel <friedel@we-build.city>
2019-10-23 15:37:33 +0200

    - added ie11 compatibility

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
+// babel.config.js
 module.exports = {
   presets: [
-    '@vue/app'
+    ['@vue/app', {
+      polyfills: [
+        'es6.promise',
+        'es6.symbol',
+        'es7.object.entries'
+      ]
+    }]
   ]
 }
